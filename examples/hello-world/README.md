d3477f8e12ea86a7da61f784f27df4a51e466e2f
Gayan Witharana <gayan@thinkcube.com>
2016-10-11 10:28:58 +0530

    add example using Create React App

---
+++ examples/hello-world/README.md
@@ -0,0 +1,15 @@
+React Google Login Component Example
+==============================
+
+This is a very simple — yet runnable app — showing how to use React Google Login component.
+
+## Running Example
+
+**In the project directory, run:**
+```
+$ npm install
+$ npm start
+```
+**Open [http://localhost:3000](http://localhost:3000) to view it in the browser.**
+
+[User Guide](http://localhost:3000)
---
+++ examples/hello-world/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { GoogleLogin } from 'react-google-login-component';
+
+class App extends Component {
+  responseGoogle (googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log({accessToken: id_token});
+    //anything else you want to do(save to localStorage)...
+  }
+
+  render () {
+    return (
+      <div>
+        <GoogleLogin socialId="yourClientID"
+                     class="google-login"
+                     scope="profile"
+                     responseHandler={this.responseGoogle}
+                     buttonText="Login With Google"/>
+      </div>
+    );
+  }
+}
+
+export default App;
