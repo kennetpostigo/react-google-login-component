6e1e7bfe9d3bd2c094b7e952db51cedebdae6489
Darren Chiu <darren@stakkfactory.com>
2017-04-11 14:23:19 +0800

    init the gapi once so that it can chrome will not block the login popup

--- src/GoogleLogin.js
+++ src/GoogleLogin.js
@@ -1,44 +1,54 @@
 import React from 'react';
 
-export default class GoogleLogin extends React.Component{
-  constructor(props) {
+var gapi;
+var auth2;
+
+export default class GoogleLogin extends React.Component {
+  constructor (props) {
     super(props);
   }
 
   componentDidMount () {
-    (function(d, s, id){
-     var js, gs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = 'https://apis.google.com/js/platform.js'
-     gs.parentNode.insertBefore(js, gs);
-   }(document, 'script', 'google-platform'));
+    window.googlePlatformReadyCb = () => {
+      console.info('gapi ready.');
+      window.gapi.load('auth2', function () {
+        var auth2 = window.gapi.auth2.init({
+          client_id: socialId,
+          fetch_basic_profile: false,
+          scope: scope
+        });
+      });
+    };
+
+    (function (d, s, id) {
+      var js, gs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://apis.google.com/js/platform.js?onload=googlePlatformReadyCb';
+      gs.parentNode.insertBefore(js, gs);
+    }(document, 'script', 'google-platform'));
+
+    var socialId = this.props.socialId,
+      responseHandler = this.props.responseHandler,
+      scope = this.props.scope;
   }
 
   checkLoginState (response) {
     if (auth2.isSignedIn.get()) {
       var profile = auth2.currentUser.get().getBasicProfile();
     } else {
-      if(this.props.responseHandler) {
+      if (this.props.responseHandler) {
         this.props.responseHandler({status: response.status});
       }
     }
   }
 
   clickHandler () {
-    var socialId = this.props.socialId,
-        responseHandler = this.props.responseHandler,
-        scope = this.props.scope;
+    var auth2 = window.gapi.auth2.getAuthInstance();
 
-    gapi.load('auth2', function() {
-      var auth2 = gapi.auth2.init({
-        client_id: socialId,
-        fetch_basic_profile: false,
-        scope: scope
-      });
-      auth2.signIn().then(function(googleUser) {
-        responseHandler(googleUser);
-      });
+    auth2.signIn().then((googleUser) => {
+      this.props.responseHandler(googleUser);
     });
   }
 
@@ -46,9 +56,9 @@ export default class GoogleLogin extends React.Component{
     return (
       <div>
         <button className={this.props.class} onClick={ this.clickHandler.bind(this) }>
-            {this.props.buttonText}
+          {this.props.buttonText}
         </button>
       </div>
-    )
+    );
   }
-}
+};
